9898b06e17bacd80498b46e483127cd05908a9bf
pbodsk <me@peterbodskov.dk>
2014-03-25 20:59:05 +0100

    work continued on tutorial

--- RWReactivePlayground/RWViewController.m
+++ RWReactivePlayground/RWViewController.m
@@ -17,8 +17,6 @@
 @property (weak, nonatomic) IBOutlet UIButton *signInButton;
 @property (weak, nonatomic) IBOutlet UILabel *signInFailureText;
 
-@property (nonatomic) BOOL passwordIsValid;
-@property (nonatomic) BOOL usernameIsValid;
 @property (strong, nonatomic) RWDummySignInService *signInService;
 
 @end
@@ -28,14 +26,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [self updateUIState];
-    
     self.signInService = [RWDummySignInService new];
-    
-    // handle text changes for both text fields
-    [self.usernameTextField addTarget:self action:@selector(usernameTextFieldChanged) forControlEvents:UIControlEventEditingChanged];
-    [self.passwordTextField addTarget:self action:@selector(passwordTextFieldChanged) forControlEvents:UIControlEventEditingChanged];
-    
+        
     // initially hide the failure message
     self.signInFailureText.hidden = YES;
     
@@ -65,6 +57,31 @@
     RAC(self.passwordTextField, backgroundColor) = [validPasswordSignal map:^id(NSNumber *passwordValid) {
         return passwordValid.boolValue ? [UIColor clearColor] : [UIColor yellowColor];
     }];
+    
+    RACSignal *signUpActiveSignal = [RACSignal combineLatest:@[validUserNameSignal, validPasswordSignal] reduce:^id(NSNumber *userNameValid, NSNumber *passwordValid){
+        return @(userNameValid.boolValue && passwordValid.boolValue);
+    }];
+    
+    [signUpActiveSignal subscribeNext:^(NSNumber *signupActive) {
+        self.signInButton.enabled = signupActive.boolValue;
+    }];
+    
+    [[[[self.signInButton rac_signalForControlEvents:UIControlEventTouchUpInside]
+      doNext:^(id x) {
+          self.signInButton.enabled = NO;
+          self.signInFailureText.hidden = YES;
+      }]
+      flattenMap:^id(id value) {
+          return [self signInSignal];
+      }]
+     subscribeNext:^(NSNumber *signedIn) {
+         self.signInButton.enabled = YES;
+         BOOL success = signedIn.boolValue;
+         self.signInFailureText.hidden = success;
+         if(success){
+             [self performSegueWithIdentifier:@"signInSuccess" sender:self];
+         }
+     }];
 }
 
 - (BOOL)isValidUsername:(NSString *)username {
@@ -75,40 +92,14 @@
     return password.length > 3;
 }
 
-- (IBAction)signInButtonTouched:(id)sender {
-    // disable all UI controls
-    self.signInButton.enabled = NO;
-    self.signInFailureText.hidden = YES;
-    
-    // sign in
-    [self.signInService signInWithUsername:self.usernameTextField.text
-                                  password:self.passwordTextField.text
-                                  complete:^(BOOL success) {
-                                      self.signInButton.enabled = YES;
-                                      self.signInFailureText.hidden = success;
-                                      if (success) {
-                                          [self performSegueWithIdentifier:@"signInSuccess" sender:self];
-                                      }
-                                  }];
-}
-
-
-// updates the enabled state and style of the text fields based on whether the current username
-// and password combo is valid
-- (void)updateUIState {
-//    self.usernameTextField.backgroundColor = self.usernameIsValid ? [UIColor clearColor] : [UIColor yellowColor];
-//    self.passwordTextField.backgroundColor = self.passwordIsValid ? [UIColor clearColor] : [UIColor yellowColor];
-    self.signInButton.enabled = self.usernameIsValid && self.passwordIsValid;
-}
-
-- (void)usernameTextFieldChanged {
-    self.usernameIsValid = [self isValidUsername:self.usernameTextField.text];
-    [self updateUIState];
-}
-
-- (void)passwordTextFieldChanged {
-    self.passwordIsValid = [self isValidPassword:self.passwordTextField.text];
-    [self updateUIState];
+- (RACSignal *)signInSignal {
+    return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [self.signInService signInWithUsername:self.usernameTextField.text password:self.passwordTextField.text complete:^(BOOL success) {
+            [subscriber sendNext:@(success)];
+            [subscriber sendCompleted];
+        }];
+        return nil;
+    }];
 }
 
 @end
